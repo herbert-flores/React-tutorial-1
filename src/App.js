763b14a7e7bae1747a4b6d39d45924379c1612c2
herbert flores <herbertmff@hotmail.com>
2021-06-06 13:11:05 +1000

    styling css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 function App() {
-    return <div>hello mate!!</div>;
+    return (
+        <div>
+            <h1>My todos</h1>
+            <div className="card">
+                <h2>Title</h2>
+                <div className="actions">
+                    <button className="btn">Delete</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
-\
