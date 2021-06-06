5031b2efc1b1af7c3e3e01c3c60781ba32e48232
herbert flores <herbertmff@hotmail.com>
2021-06-06 13:24:12 +1000

    props dynamic content

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
     return (
         <div>
             <h1>My todos</h1>
-            <Todo />
-            <Todo />
-            <Todo />
+            <Todo text="Learn React" />
+            <Todo text="Master React" />
+            <Todo text="Explore the full react" />
         </div>
     );
 }
